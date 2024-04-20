d754cb68aaf62872b4d406fc7c635717e73667cb
0xSynon <dev@devhunt.eu>
2024-04-20 15:29:08 +0200

    Added option --strip-init

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const { disassemble } = require('./lib/disassemble');
 const { format } = require('./lib/format');
 
 function displayHelp() {
-  console.log("Usage:");
-  console.log("  huffc /path/to/contract.huff --bytecode | bc2oc  # Convert bytecode to opcodes");
-  console.log("  bc2oc                                            # Display this help message");
+  console.log('Usage:');
+  console.log('  huffc contract.huff --bytecode | bc2oc [options] # Convert bytecode to opcodes');
+  console.log('  bc2oc                                            # Display this help message');
+  console.log('');
+  console.log('Options:');
+  console.log('  --strip-init           Convert initialization code to runtime');
+  console.log('');
 }
 
 if (process.stdin.isTTY) {
@@ -14,7 +18,8 @@ if (process.stdin.isTTY) {
   process.exit(0);
 }
 
-process.stdin.setEncoding('utf8');
+process.argv.shift();
+process.argv.shift();
 
 const chunks = [];
 process.stdin.on('readable', () => {
@@ -25,7 +30,14 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  const bytecode = chunks.join('');
+  let bytecode = chunks.join('');
+  if (process.argv.includes('--strip-init')) {
+    const index = bytecode.indexOf('3d393df3');
+    if (index === -1) {
+      throw new Error('Could not locate initialization');
+    }
+    bytecode = bytecode.slice(index + 8);
+  }
   const disassembled = disassemble(bytecode);
   console.log(format(disassembled));
 });
